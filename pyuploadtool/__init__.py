85d5ab94bfda17f40b9be9d2bf878c1eb9ba20f7
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-19 15:48:33 +0300

    fix: remove redundant imports

--- pyuploadtool/__init__.py
+++ pyuploadtool/__init__.py
@@ -1,6 +1,7 @@
 from .types import BuildType
-from .metadata import ReleaseMetadata, update_metadata_with_user_specified_data
+from .metadata import ReleaseMetadata
+from .metadata import update_metadata_with_user_specified_data  # noqa (fixes import issue)
 from .build_systems import BuildSystemFactory
 from .releases_hosting_provider import ReleasesHostingProviderFactory
 
-__all__ = (ReleaseMetadata, BuildSystemFactory, ReleasesHostingProviderFactory)
+__all__ = (ReleaseMetadata, BuildSystemFactory, ReleasesHostingProviderFactory, BuildType)
